ac00adc158ddddaac3dc23f1b74460d792d5b997
adibrisan <adi.brisan@yahoo.com>
2020-05-18 22:33:18 +0300

    PROG-20

--- src/main/java/PROG_8/RecenzieFrame.java
+++ src/main/java/PROG_8/RecenzieFrame.java
@@ -27,6 +27,31 @@ import org.json.simple.parser.ParseException;
 
 import PROG_11.ClientFrame;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.Border;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class RecenzieFrame extends JFrame{
 	private RecenzieTextPanel textPanel;
 	private ClientFrame clientFrame;
@@ -47,11 +72,84 @@ public class RecenzieFrame extends JFrame{
 		ToolBar2Btn toolBar = textPanel.getToolBar();
 		toolBar.setBackground(culoare);
 		
+		toolBar.getOKBtn().addActionListener(new ActionListener() {
+
+			@SuppressWarnings("unchecked")
+			public void actionPerformed(ActionEvent e) {
+				
+				File file = new File("src/main/resources/FisierRecenzii.json");
+				
+				String recenzie = textPanel.getTextArea().getText();
+				String numeClient = clientFrame.getNumeClient();
+				
+				JSONParser parser = new JSONParser();
+
+				try (Reader reader = new FileReader(file)) {
+					JSONArray jsonArray = (JSONArray) parser.parse(reader);
+					if(jsonArray.isEmpty()) {
+						
+						JSONObject obj1 = new JSONObject();
+						obj1.put("Nume Client", numeClient);
+						obj1.put("Recenzie", recenzie);
+
+
+						JSONArray list = new JSONArray();
+						list.add(obj1);
+
+
+						try (FileWriter fw = new FileWriter(file)) {
+							fw.write(list.toJSONString());
+						} catch (IOException e1) {e1.printStackTrace();}
+						
+						JTextArea textArea = textPanel.getTextArea();
+						textArea.setText("");
+						
+						return;
+					}
+					else {
+						JSONParser parser2 = new JSONParser();
+						try (Reader reader2 = new FileReader(file)) {
+							JSONArray jsonArray2 = (JSONArray) parser.parse(reader2);
+							JSONObject obj1 = new JSONObject();
+							obj1.put("Nume Client", numeClient);
+							obj1.put("Recenzie", recenzie);
+
+							jsonArray2.add(obj1);
+
+							try (FileWriter fw = new FileWriter(file)) {
+								fw.write(jsonArray2.toJSONString());
+							} catch (IOException e1) {
+								e1.printStackTrace();
+							}
+							
+							JTextArea textArea = textPanel.getTextArea();
+							textArea.setText("");
+							
+						} catch (IOException e1) {e1.printStackTrace();}
+						  catch (ParseException e1) {e1.printStackTrace();}
+					}
+
+					}catch (IOException e1) {e1.printStackTrace();}
+					 catch (ParseException e1) {e1.printStackTrace();}
+            }
+			
+		});
+		
+
+		
 		setSize(400,300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 		setLocationRelativeTo(null);
 
+	}
+	
+
+	
+	public void setClientFrameOn(ClientFrame client) {
+		this.clientFrame = client;
+		clientFrame.setVisible(true);
+		setVisible(false);
 	}
 	
 	public RecenzieFrame getRecenzieFrame() {
@@ -66,3 +164,4 @@ public class RecenzieFrame extends JFrame{
 
 
 }
+
