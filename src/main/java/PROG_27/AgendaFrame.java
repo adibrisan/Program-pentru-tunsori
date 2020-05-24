6d042799862afc12317cd86cd1acf16d92c3798b
adibrisan <adi.brisan@yahoo.com>
2020-05-24 21:08:51 +0300

    PROG-28

--- src/main/java/PROG_27/AgendaFrame.java
+++ src/main/java/PROG_27/AgendaFrame.java
@@ -1,19 +1,32 @@
 package PROG_27;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import PROG_11.ClientFrame;
+import PROG_8.ToolBar2Btn;
 
 public class AgendaFrame extends JFrame{
 	private ClientFrame clientFrame;
+	private ToolBar2Btn toolBar;
+	private AgendaPanel agendaPanel;
 	
 	public AgendaFrame() {
 		super("Agenda");
-		
 		setLayout(new BorderLayout());
+		toolBar = new ToolBar2Btn();
+		agendaPanel = new AgendaPanel();
+		add(toolBar, BorderLayout.SOUTH);
+		add(agendaPanel, BorderLayout.CENTER);
+		
+		Color culoare = new Color(44, 62, 80);
+		
+		agendaPanel.setBackground(culoare);		
+		toolBar.setBackground(culoare);
 		
 		setSize(400,300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/main/java/PROG_27/AgendaFrizerFrame.java
+++ src/main/java/PROG_27/AgendaFrizerFrame.java
@@ -1,17 +1,28 @@
 package PROG_27;
 import java.awt.BorderLayout;
+import java.awt.Color;
 
 import javax.swing.JFrame;
 
 import PROG_11.AdminFrame;
+import PROG_8.ToolBar2Btn;
 
 public class AgendaFrizerFrame extends JFrame{
 	private AdminFrame adminFrame;
-	
+	private ToolBar2Btn toolBar;
+	private AgendaPanel agendaPanel;
 	public AgendaFrizerFrame() {
 		super("Agenda");
-		
 		setLayout(new BorderLayout());
+		toolBar = new ToolBar2Btn();
+		agendaPanel = new AgendaPanel();
+		add(toolBar, BorderLayout.SOUTH);
+		add(agendaPanel, BorderLayout.CENTER);
+		
+		Color culoare = new Color(44, 62, 80);
+		
+		agendaPanel.setBackground(culoare);
+		toolBar.setBackground(culoare);
 		
 		setSize(400,300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
