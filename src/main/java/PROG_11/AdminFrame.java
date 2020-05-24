7ef8480b9efdcfecd57616f01953a88a2b77426d
adibrisan <adi.brisan@yahoo.com>
2020-05-24 13:01:17 +0300

    added btn recenzii

--- src/main/java/PROG_11/AdminFrame.java
+++ src/main/java/PROG_11/AdminFrame.java
@@ -4,22 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.Iterator;
-
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
+import PROG_6.RecenziiFrame;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
 
 public class AdminFrame extends JFrame{
 	private LoginFrame loginFrame;
@@ -38,6 +26,13 @@ public class AdminFrame extends JFrame{
 		
 		optiuniPanel.setBackground(culoare);
 		
+		optiuniPanel.getRecenziiBtn().addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				RecenziiFrame recenziiFrame = new RecenziiFrame();
+				recenziiFrame.setAdminFrameOff(getAdminFrame());
+			}
+		});
 		
 		setVisible(true);
 		setSize(400,300);
